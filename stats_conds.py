407d8d840178f50f757714fa66aca2121e3b1a68
fabriziomonge <37080465+fabriziomonge@users.noreply.github.com>
2020-08-11 10:05:43 +0200

    Update stats_conds.py

--- stats_conds.py
+++ stats_conds.py
@@ -83,11 +83,13 @@ except:
     Verrà considerato l'andamento del ticker di default...
     """)
     
-    df = pdr.get_data_yahoo('VTI', start = '1980-1-1')['Close']
-    df = df.resample('M').last()
-    df = pd.DataFrame(df)
-    df['index']=df.index
-    df = df.set_index('index',1)
+#     df = pdr.get_data_yahoo('VTI', start = '1980-1-1')['Close']
+#     df = df.resample('M').last()
+#     df = pd.DataFrame(df)
+#     df['index']=df.index
+#     df = df.set_index('index',1)
+
+    titolo = "VTI"
 
     
 
