971cbd69b4ef08c58d3a83b82c6dd954c0ce2bc5
Fabrizio Monge <37080465+fabriziomonge@users.noreply.github.com>
2020-08-24 11:37:12 +0200

    Update stats_conds.py

--- stats_conds.py
+++ stats_conds.py
@@ -98,43 +98,43 @@ try:
 
 except:
             
-    try:
-            client = Client('http://ws.bbfinance.net/_bnb_ws/bnb_ws.wsdl') #4G58NHO2VNH3RJD9
-            LOGIN = client.service.Login("fabrizio.monge@gmail.com", "PMuxLBzkFvRib21")
-            TOK = LOGIN.LOGIN_RESPONSE.token
+#     try:
+     client = Client('http://ws.bbfinance.net/_bnb_ws/bnb_ws.wsdl') #4G58NHO2VNH3RJD9
+     LOGIN = client.service.Login("fabrizio.monge@gmail.com", "PMuxLBzkFvRib21")
+     TOK = LOGIN.LOGIN_RESPONSE.token
 
-            A = client.service.GetHistoryFromDate(TOK,"SP", titolo,'1980-1-1', "")
-            B = client.service.GetSymbol(TOK,"SP", titolo)
-            try:
-              length = len(A.QUOTES2)
-            except:
-              length= 0
+      A = client.service.GetHistoryFromDate(TOK,"SP", titolo,'1980-1-1', "")
+      B = client.service.GetSymbol(TOK,"SP", titolo)
+      try:
+         length = len(A.QUOTES2)
+      except:
+         length= 0
 
-            lista1=[]
-            lista2=[]
+       lista1=[]
+       lista2=[]
 
-            for t in range (length):
-              data = A.QUOTES2[t].d
-              prezzo = A.QUOTES2[t].c
-              lista1.append(data)
-              lista2.append(prezzo)
+        for t in range (length):
+           data = A.QUOTES2[t].d
+           prezzo = A.QUOTES2[t].c
+           lista1.append(data)
+           lista2.append(prezzo)
 
-            df = pd.DataFrame(lista2, index = lista1, columns=[titolo])
+         df = pd.DataFrame(lista2, index = lista1, columns=[titolo])
             
-    except:
+#     except:
             
-              st.write("""
-              ## Attenzione: il ticker inserito non è stato trovato!
-              Verrà considerato l'andamento del ticker di default...
-              """)
+#               st.write("""
+#               ## Attenzione: il ticker inserito non è stato trovato!
+#               Verrà considerato l'andamento del ticker di default...
+#               """)
     
-              df = pdr.get_data_yahoo('^GSPC', start = '1980-1-1')['Close']
-              df = df.resample('M').last()
-              df = pd.DataFrame(df)
-              df['index']=df.index
-              df = df.set_index('index',1)
+#               df = pdr.get_data_yahoo('^GSPC', start = '1980-1-1')['Close']
+#               df = df.resample('M').last()
+#               df = pd.DataFrame(df)
+#               df['index']=df.index
+#               df = df.set_index('index',1)
 
-              titolo = "^GSPC"
+#               titolo = "^GSPC"
     
 df['indice']= df.index
 df['indice'] = df['indice'].dt.strftime('%Y-%m')
